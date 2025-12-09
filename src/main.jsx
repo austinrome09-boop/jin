9df10e3205ddf06e88dde8056cda7e2380a5f6a2
austinrome09-boop <austinrome09@gmail.com>
2025-12-10 01:13:10 +0800

    Updated some text

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { HashRouter } from 'react-router-dom' // Import HashRouter
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* Use HashRouter specifically for GitHub Pages */}
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>,
 )
