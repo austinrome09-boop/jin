69a59e49acecef566febb88108a676be7b008e82
austinrome09-boop <austinrome09@gmail.com>
2025-12-10 00:48:31 +0800

    Add CNAME and prepare for fresh deploy

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-// Change this import:
-import { HashRouter } from 'react-router-dom' 
+import { HashRouter } from 'react-router-dom' // Import HashRouter
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* Change this tag: */}
+    {/* Use HashRouter specifically for GitHub Pages */}
     <HashRouter>
       <App />
     </HashRouter>
